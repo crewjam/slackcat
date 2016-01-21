9bfd7d57477a92c8008ceaddf7a38b2a4b12f974
Ross Kinder <ross@kndr.org>
2016-01-21 14:16:34 -0500

    add -tee, -read, -write switches

--- slackcat.go
+++ slackcat.go
@@ -6,15 +6,26 @@ import (
 	"os"
 	"os/exec"
 
-	"github.com/crewjam/wx/slackcat/slackio"
+	"github.com/crewjam/slackcat/slackio"
 )
 
-var SlackToken = flag.String("slack-token", "", "Slack Token")
-var SlackURL = flag.String("slack-url", "", "Slack URL (may be empty)")
-var Channel = flag.String("channel", "general", "The name of the slack channel")
-
 func main() {
+	token := flag.String("token", "", "Slack Token")
+	endpoint := flag.String("endpoint", "", "Slack URL (may be empty)")
+	channel := flag.String("channel", "general", "The name of the slack channel")
+	doTee := flag.Bool("tee", false, "tee stdin to both stdout and slack")
+	doRead := flag.Bool("read", false, "only read from slack, don't also write. Default is to both read and write.")
+	doWrite := flag.Bool("write", false, "only write to slack, don't also read. Default is to both read and write.")
+
 	flag.Parse()
+	if !*doRead && !*doWrite {
+		*doRead = true
+		*doWrite = true
+	}
+	if *doTee {
+		*doWrite = true
+		*doRead = false
+	}
 
 	s, err := slackio.New(*SlackURL, *SlackToken, *Channel)
 	if err != nil {
@@ -22,18 +33,39 @@ func main() {
 	}
 	defer s.Close()
 
+	var stdin io.Reader = os.Stdin
+	if *doTee {
+		stdin = io.TeeReader(os.Stdin, os.Stdout)
+	}
+
 	// copy from stdin to slack and slack to stdout
 	if len(flag.Args()) == 0 {
-		go io.Copy(os.Stdout, s)
-		io.Copy(s, os.Stdin)
+		if *doWrite && *doRead {
+			go io.Copy(os.Stdout, s)
+			io.Copy(s, stdin)
+		} else if *doWrite {
+			io.Copy(s, stdin)
+		} else if *doRead {
+			io.Copy(os.Stdout, s)
+		}
 		return
 	}
 
 	// execute the program and bind to stdin/stdout
 	cmd := exec.Command(flag.Args()[0], flag.Args()[1:]...)
-	cmd.Stdout = s
-	cmd.Stderr = s
-	cmd.Stdin = s
+	if *doWrite {
+		cmd.Stdout = s
+		cmd.Stderr = s
+	} else {
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+	}
+	if *doRead {
+		cmd.Stdin = s
+	} else {
+		cmd.Stdin = stdin
+	}
+
 	err = cmd.Run()
 	if err != nil {
 		panic(err)
